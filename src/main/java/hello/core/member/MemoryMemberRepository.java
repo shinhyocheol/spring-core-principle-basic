a987f76067de5fc2ecf8dbca5328e0401e3b3814
shinhyocheol <zzzskssk1@naver.com>
2022-07-23 13:38:08 +0900

    # 프로젝트 커밋

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실무에서는 HashMap 보다 ConcurrentHashMap을 사용하는게 더 좋다.(동시성 이슈)
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
