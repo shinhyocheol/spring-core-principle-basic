5da6eead625c357e319035e6e69ecc1ce8965915
shinhyocheol <zzzskssk1@naver.com>
2022-07-30 22:56:00 +0900

    #

--- src/main/java/com/order/example/MemberApp.java
+++ src/main/java/com/order/example/MemberApp.java
@@ -10,7 +10,7 @@ public class MemberApp {
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
         Member memberA = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(memberA);   
+        memberService.join(memberA);
 
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + memberA.getName());
