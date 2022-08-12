2147137c3f3ce2d0f82150a9c5777643c3a3853e
shinhyocheol <zzzskssk1@naver.com>
2022-08-13 00:39:59 +0900

    # 빈 메타 정보 공부

--- src/main/java/com/core/example/MemberApp.java
+++ src/main/java/com/core/example/MemberApp.java
@@ -18,7 +18,7 @@ public class MemberApp {
          * 이전에는 필요한 객체를 'AppConfig' 를 사용해서 직접 조회했지만, 이제부터는 스프링 컨테이너를 통해서 필요한 스프링 빈(객체)를 찾아야 한다.
          * 스프링 빈은 'applicationContext.getBean()을 통해서 찾을 수 있다.
          * 기존에는 개발자가 직접 자바코드로 모든 것을 했다면 이제부터는 스프링 컨테이너에 객체를 스프링 빈으로 등록하고,
-         * 스프링 컨테이너에서 스프링 빈을 찾아서 사용하도록 변경되었다. 
+         * 스프링 컨테이너에서 스프링 빈을 찾아서 사용하도록 변경되었다.
         */
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
