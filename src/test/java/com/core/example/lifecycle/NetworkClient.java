13aeff148b273df1cd166bd8fed2faad6f43ce3b
shinhyocheol <zzzskssk1@naver.com>
2022-09-24 19:45:32 +0900

    # 스프링 라이프사이클 공부

--- src/test/java/com/core/example/lifecycle/NetworkClient.java
+++ src/test/java/com/core/example/lifecycle/NetworkClient.java
@@ -1,14 +1,22 @@
 package com.core.example.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * "초기화, 소멸 인터페이스 단점"
+ *  - 이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스 의존한다.
+ *  - 초기화, 소멸 메서드의 이름을 변경할 수 없다.
+ *  - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
+ *
+ *  참고 : 인터페이스를 사용하는 초기화, 종료 방법은 스프링 초창기에 나온 방법들이고, 지금은 더나은 방법들이 있어서 거의 사용하지 않는다.
+ */
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-
     }
 
     public void setUrl(String url) {
@@ -27,5 +35,18 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
