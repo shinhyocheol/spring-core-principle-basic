4c2c91d428ee0971a25594b01c566b707a231a5d
shinhyocheol <zzzskssk1@naver.com>
2024-06-19 00:13:56 +0900

    study: scope 레벨 request 로 설정 후 실행 테스트 (현재 단계 실패)

---
+++ src/main/java/com/core/example/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package com.core.example.web;
+
+import com.core.example.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
